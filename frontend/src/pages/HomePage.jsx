d19b47ca8d2101d8dc4bf0c6730bff487fb0f8d3
Sebastian Peñaranda Hurtado <127699683+Heroslaver-tech@users.noreply.github.com>
2023-10-31 18:00:13 -0500

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const HomePage = () => {
   return (
     <div>
-      <h1>¡Encuentra aquí tu destino ideal con nuestro sencillo cuestionario!</h1>
+      <h1>¡EEncuentra aquí tu destino ideal con nuestro sencillo cuestionario!</h1>
     </div>
   )
 }
