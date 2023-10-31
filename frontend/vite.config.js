e9341cd8f9c540f1932451a56c92ad66be5528b3
Sebastian Peñaranda Hurtado <127699683+Heroslaver-tech@users.noreply.github.com>
2023-10-31 17:54:23 -0500

    Update container.test.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
   plugins: [react()],
   server: {
     port: 3000,
