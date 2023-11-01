1460ee7e03a2e14682f3d25cdad5a21b2d87cf0f
Juan Pablo <carabali.juan@correounivalle.edu.co>
2023-10-31 23:28:38 -0500

    test

--- backend/five_to_fly/usuarios/models.py
+++ backend/five_to_fly/usuarios/models.py
@@ -5,6 +5,8 @@ class UserProfile(models.Model):
     nombre = models.CharField(max_length=100)
     correo = models.EmailField(unique=True)
     celular = models.CharField(max_length=15)
+    contrasena = models.CharField(max_length=255)
+    gender = models.CharField(max_length=1, null=True, blank=True)
     direccion = models.CharField(max_length=255, null=True, blank=True)  # El uso de 'null=True' y 'blank=True' permite que el campo sea opcional.
     ciudad = models.CharField(max_length=100, null=True, blank=True)
 
