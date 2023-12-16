8c99d57fe921d1d4d4f0da4f6247615f5ea1140c
juan david iron-mind <davidtovarmontoya@gmail.com>
2023-12-16 17:34:01 -0500

    landing image

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
 
 export const HomePage = () => {
   return (
-    <article className='flex flex-wrap mt-8 mx-auto bg-white justify-center items-center rounded-xl  sm:w-2/4 h-auto'>
-      <div className='flex justify-center items-center flex-col'>
-        <h1 className='flex justify-center text-3xl font-bold p-4'>¡Encuentra aquí tu destino ideal con nuestro sencillos cuestionarios!.</h1>
-        <p className='flex justify-center text-xl p-4'>Inicia sesión para saber tu proximo paraíso, o regístrate si aun no lo has hecho.</p>
-      </div>
-    </article>
-  )
+		<article className="flex flex-wrap mt-8 mx-auto bg-white justify-center items-center rounded-xl  sm:w-2/4 h-auto">
+			<div className="flex justify-center items-center flex-col">
+				<h1 className="flex justify-center text-3xl font-bold p-4">
+					¡Encuentra aquí tu destino ideal con nuestro sencillos cuestionarios!.
+				</h1>
+				<img
+					src="https://imgs.search.brave.com/_m7dc-s3ORR7sFIjN39YroX55_m74r1F4OHfYlSj5vc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudmV4ZWxzLmNv/bS9tZWRpYS91c2Vy/cy8zLzMwODYvcmF3/L2QxZWZjMzc5ZDQ5/MTcxY2RhN2E0NTVl/NzdmY2NmNDFmLXBh/cXVldGUtZGUtaWNv/bm9zLWRlLXZpYWpl/LmpwZw"
+					alt="viajes"
+				/>
+				<p className="flex justify-center text-xl p-4">
+					Inicia sesión para saber tu proximo paraíso, o regístrate si aun no lo
+					has hecho.
+				</p>
+			</div>
+		</article>
+	);
 }
